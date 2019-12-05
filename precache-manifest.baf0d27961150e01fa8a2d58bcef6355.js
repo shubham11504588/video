8271044908c5c5163e7c487168b24eaf2bac5244
shubham11504588 <shubhamanand.dls@gmail.com>
2019-12-05 14:39:36 +0530

    Updates

---
+++ precache-manifest.baf0d27961150e01fa8a2d58bcef6355.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44f6de32941efeefc2baf4e21def4ffc",
+    "url": "/video/./index.html"
+  },
+  {
+    "revision": "df1088415961713313e2",
+    "url": "/video/./static/css/main.e988cc1c.chunk.css"
+  },
+  {
+    "revision": "b17879981cc2e160bbb4",
+    "url": "/video/./static/js/2.2ddcab89.chunk.js"
+  },
+  {
+    "revision": "df1088415961713313e2",
+    "url": "/video/./static/js/main.a94f9ab9.chunk.js"
+  },
+  {
+    "revision": "0e558f0cc6d72ac74bcc",
+    "url": "/video/./static/js/runtime~main.ab87a6e3.js"
+  }
+]);
